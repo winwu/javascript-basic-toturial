67b5f3409ffdb9d30665fa269083fa12166e87ca
winwu <yiyingwu.1990@gmail.com>
2015-09-21 20:15:45 +0800

    add files

---
+++ week2/demo/js/01-ans.js
@@ -0,0 +1,14 @@
+// 練習 console
+
+var course = 'javascript week2';
+
+var teacher = {
+  name: 'winwu',
+  website: 'blog.winwu.today'
+};
+
+console.log('debug log');
+console.debug('today course: ' + course);
+console.dir(teacher);
+console.error("這是我們自己製作的 error message");
+console.info("info message");
